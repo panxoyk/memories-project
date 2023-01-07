de44ed31bfc1e2af5d4c392e4a10ffafcf9c1077
panncho <francisco.castilloh@usm.cl>
2023-01-07 18:12:18 -0300

    Proyecto primeros pasos

---
+++ client/src/components/Posts/Post/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './post.css';
+
+function Post() {
+    return (
+        <h1> Post </h1>
+    );
+};
+
+export default Post;
--- server/index.js
+++ server/index.js
@@ -3,10 +3,22 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
+app.use('/posts', postRoutes);
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.urlencoded(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-conosle.log('hola')
+// https://cloud.mongodb.com
+
+const CONNECTION_URL = 'mongodb+srv://Francisco:Fach0018@cluster0.xqzrlqv.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+const client = mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+client.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
